4989415350b91fdb1487bd5d17361ac039a820b9
V0odo0Z <jagaloren@gmail.com>
2026-02-11 16:36:08 +0100

    Working on ImportMenu to fill Productos table at the DB from a .csv ProductImporter finished(?)

--- app/src/main/java/com/example/barrel_la_fama/ImportMenu.java
+++ app/src/main/java/com/example/barrel_la_fama/ImportMenu.java
@@ -1,14 +1,30 @@
 package com.example.barrel_la_fama;
 
+import static android.content.Intent.ACTION_OPEN_DOCUMENT;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ImportMenu extends AppCompatActivity {
+    Uri uri;
+    Button aceptarBotton;
+    EditText uriEt;
+    //Actividad que espera un resultado. La uso para lanzar el explorador de archivos y esperar el uri
+    private ActivityResultLauncher<String[]> filePickerLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,38 @@ public class ImportMenu extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Defino el ActivityResultLauncher. Como parámetros le paso el tipo de resultado que espero (con ActivityResultContracts.OpenDocument()) y la variable donde guardarlo (uri en este caso, que es lo que busco/quiero obtener) y como obtenerlo (con una lambda)
+        filePickerLauncher = registerForActivityResult(new ActivityResultContracts.OpenDocument(), uri -> {
+            if (uri != null){
+                Log.d("URI recibida:", uri.toString());
+                this.uri = uri;
+                uriEt.setText(uri.toString());
+            }
+        });
+        uriEt = findViewById(R.id.uriCsv);
+        aceptarBotton = findViewById(R.id.boton_aceptar_uri);
+        uriEt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectFile();
+
+            }
+        });
+
+        aceptarBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aqui recoger la uri, abrir el archivo, trocearlo e ir metiendo los productos en la bbdd
+                ProductImporter pi = new ProductImporter(uri);
+            }
+        });
+    }
+
+    //Metodo para que salga el selector de archivos y me devuelva la uri.
+    public void selectFile(){
+        //filePickerLauncher.launch(new String[]{"text/csv", "application/pdf"});
+
+        //El filtro new String[]{} solo funciona de esta manera. Poniendo text/csv o text/.csv deja ver el archivo, pero no seleccionarlo
+        filePickerLauncher.launch(new String[]{"text/*"});
     }
 }
--- app/src/main/java/com/example/barrel_la_fama/MainActivity.java
+++ app/src/main/java/com/example/barrel_la_fama/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.barrel_la_fama;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     AppDatabase db;
+    Button anadirItemBt, importarCartaBt, actualizarBBDDBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         db = AppDatabase.getInstance(getApplicationContext());
+        anadirItemBt = findViewById(R.id.anadir_item);
+        importarCartaBt = findViewById(R.id.importarCarta);
+
+        importarCartaBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ImportMenu.class);
+                startActivity(i);
+            }
+        });
         //Hay que tirarlo con un hilo independiente para evitar el cap del UI Thread de Room
         //Uso lambdas para facilitarlo
         Producto p1 = new Producto("ProductoPrueba", "10.00", "Bebida");
@@ -56,3 +70,4 @@ public class MainActivity extends AppCompatActivity {
 }
 
 //TODO: Productos, ImportMenu, ProductImporter(finalizado?) --> Implementar importacion desde csv (¿Por que? pues porque puedo... creo)
+// --> Probar la importacion del menu
